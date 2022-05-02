d8d7b9439f7df1a406a347ff3674d6b9a583e2bd
Code Sandbox <hello@codesandbox.io>
2022-05-02 08:12:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.922a4f3c1cbdb9308b0699eee28ad7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbdaa1e463ce34cdffdd",
+    "url": "https://mateu52.github.io/csb-1lpo7p/static/css/main.c9c1e668.chunk.css"
+  },
+  {
+    "revision": "dbdaa1e463ce34cdffdd",
+    "url": "https://mateu52.github.io/csb-1lpo7p/static/js/main.7a34e2cd.chunk.js"
+  },
+  {
+    "revision": "9c479c3d5f0140a92702",
+    "url": "https://mateu52.github.io/csb-1lpo7p/static/js/runtime~main.eb4c1a76.js"
+  },
+  {
+    "revision": "356177b60c6ab8cdd040",
+    "url": "https://mateu52.github.io/csb-1lpo7p/static/js/2.8cf590a1.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "https://mateu52.github.io/csb-1lpo7p/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "565d88909f2cf38ae89b88960e2928b8",
+    "url": "https://mateu52.github.io/csb-1lpo7p/index.html"
+  }
+];
